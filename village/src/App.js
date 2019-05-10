c546fda9d8c8cd96c206f80e8006dfb0086578f1
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-10 13:25:01 -0400

    Add styles

--- village/src/App.js
+++ village/src/App.js
@@ -5,18 +5,15 @@ import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
 
-import { Router, Route, Link } from 'react-router';
+import { Route, NavLink } from 'react-router';
+import Smurf from './components/Smurf';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: {
-        id: '',
-        name: '',
-        age: '',
-        height: ''
-      }
+      smurfs: [],
+      id: null
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -24,38 +21,43 @@ class App extends Component {
   // You'll need to make sure you have the right properties on state and pass them down to props.
     componentDidMount() {
       axios
-        .get(`http://localhost:3333/smurfs/${this.props.match.params.id}`)
-        .then(res => this.setState({ item: res.data }))
+        .get(`http://localhost:3333/smurfs/`)
+        .then(res => this.setState(prevState=>{ smurfs: res.data }))
+        // console.log(res.data);
         .catch(err => console.log(err));
     }
 
-    changeHandler = ev => {
-      let value = ev.target.value;
-      const name = ev.target.name;
-      }
-  
-      this.setState(prevState => ({
-        smurf: {
-          ...prevState.smurf,
-          [name]: value
-        }
-      }));
-    };
-  
-    handleSubmit = e => {
-      e.preventDefault();
-      this.props.addSmurf(this.state.smurf);
-    };
+    
+
   
 
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+                <nav>
+          <h1 className="smurf-header">Smurfs </h1>
+          <div className="nav-links">
+            <NavLink to="/smurf-form">Add Smurf</NavLink>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </div>
+        </nav>
+
+          <Route exact path = "/" render ={(props)=><Smurfs smurfs = {this.state.smurfs} />} />
+      <Route path = "/smurf-form" render ={(props)=><SmurfForm updateSmurfs={this.updateSmurfs}/>} />
+      <Route
+    path="/smurfs/:id"
+    render={props => (
+      <Smurf {...props} smurfs={this.state.smurfs} deleteItem={this.deleteSmurf}
+        setUpdateForm={this.setUpdateForm}
+      />
+    )}
+  />
       </div>
     );
   }
+}
 
 
 export default App;
