9d8b7df035fc2928313bb23c3a19c3267b708781
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-10 16:18:51 -0400

    Bug fixes and updates for App

--- village/src/App.js
+++ village/src/App.js
@@ -7,15 +7,15 @@ import axios from 'axios';
 
 import { Route, NavLink } from 'react-router-dom';
 import Smurf from './components/Smurf';
+import { format } from 'util';
 
 
-let id = '';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
-      id: id
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -29,36 +29,60 @@ class App extends Component {
         .catch(err => console.log(err));
     }
 
+    getItemById = id => {
+      axios
+        .get(`http://localhost:3333/itemById/${id}`)
+        .then(res => this.setState({ activeSmurf: res.data }))
+        .catch(err => console.log(err));
+    };
+  
+      deleteSmurf = (e, id) => {
+        e.preventDefault();
+        axios
+          .delete(`http://localhost:3333/items/${id}`)
+          .then(res => {
+            this.setState({
+              smurfs: res.data
+            });
+            this.props.history.push(`/smurf-form/${id}`);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      };
     
-    removeSmurf =(i)=>{
-      id = this.state.smurfs[i].id
-      axios.delete(`http://localhost:3333/smurfs/${id}`)
-      .then(res=>{
-        this.setState(prevState=>({smurfs: res.data}))
-  
-      })
-      .catch(err=>console.log(err))
-    }
-    updateSmurfs = ()=>{
-      axios.get(`http://localhost:3333/smurfs`)
-        .then(res => {
-          //console.log(response.data)
-          console.log('in updatesmurfs')
-          this.setState(prevState=>({smurfs:res.data}))
-        })
-        .catch(err => console.log(err))
-    }
-    changeSmurf=(i)=>{
-      id = this.state.smurfs[i].id;
-      this.context.history.push(`/smurf-form`)
-  }
-  
+      updateSmurf = () => {
+        axios
+          .put(
+            `http://localhost:3333/items/${this.state.editingId}`,
+            this.state.item)
+          .then(res => {
+            this.setState({
+              smrufs: res.data,
+              editingId: null,
+              isEditing: false,
+              smurf: ''
+            });
+          })
+          .catch(error => console.log(error));
+      };
+    
+      setUpUpdateForm = (ev, smurf) => {
+        ev.preventDefault();
+        this.setState({
+          smurf, 
+          update: true,
+          isEditing: true,
+          editingId: smurf.id
+        });
+      };
+    
 
   render() {
     return (
       <div className="App">
           <nav>
-          <h1 className="smurf-header">Smurfs </h1>
+          <h1>Smurfs </h1>
           <div className="nav-links">
             <NavLink to="/smurf-form">Add Smurf</NavLink>
             <NavLink exact to="/">
@@ -66,21 +90,44 @@ class App extends Component {
             </NavLink>
           </div>
         </nav>
-
-          <Route exact path = "/" render ={(props)=><Smurfs smurfs = {this.state.smurfs} />} />
-      <Route path = "/smurf-form" render ={(props)=><SmurfForm updateSmurfs={this.updateSmurfs}/>} />
-      <Route
-    path="/smurfs/:id"
-    render={props => (
-      <Smurf {...props} smurfs={this.state.smurfs} deleteSmurf={this.deleteSmurf}
-        setUpdateForm={this.setUpdateForm}
-      />
-    )}
-  />
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route
+          exact
+          path="/smurfs"
+          render={props => (
+            <Smurfs
+              {...props} 
+              smurfs={this.state.smurfs}
+              getItemById={this.getItemById}
+            />
+          )}
+        />
+        <Route
+          path="/smurfs/:id"
+          render={props => (
+            <Smurf
+              {...props}
+              deleteSmurf={this.deleteSmurf}
+              smurf={this.state.activeSmurf}
+              updateSmurf={this.setUpUpdateForm}
+            />
+          )}
+        />
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm
+              {...props}
+              addSmurf={this.addSmurf}
+              updateSmurf={this.updateSmurf}
+            />
+          )}
+        />
       </div>
     );
   }
 }
+
 
 
 export default App;
